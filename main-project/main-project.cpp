902d35cfe09dfe29646951898bf818b68e9c3531
Karsiaryna <katekozlova1411@gmail.com>
2021-12-11 19:54:22 +0300

    Добавлены тестовые данные

--- main-project/main-project.cpp
+++ main-project/main-project.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #6. Wind rose\n";
     cout << "Author: Katsiaryna Kozlova\n";
+    cout << "Group: 14\n";
     return 0;
 }
